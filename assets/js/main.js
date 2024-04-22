920407a263389d3011927c0d033146ca894bbaad
Junjie Chu <65893273+Junjie-Chu@users.noreply.github.com>
2024-04-22 12:38:56 +0200

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+function sortTable(column) {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("sortable-table");
+  switching = true;
+  // 循环直到没有switching发生
+  while (switching) {
+    switching = false;
+    rows = table.rows;
+    // 遍历表格的所有行（除了标题行）
+    for (i = 1; i < (rows.length - 1); i++) {
+      shouldSwitch = false;
+      // 获取待比较的两个元素
+      x = rows[i].getElementsByTagName("TD")[column];
+      y = rows[i + 1].getElementsByTagName("TD")[column];
+      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        // 若发现一对需要交换的元素，标记为shouldSwitch并停止循环
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      // 若有标记的对，执行交换并将switching标记为true
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
